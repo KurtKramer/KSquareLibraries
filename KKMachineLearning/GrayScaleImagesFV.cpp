59dc0b4ae2643f7478e60faffcf29835a6dcd069
Kurt Kramer <kurtkramer@gmail.com>
2018-07-15 16:11:40 -0700

    Set library type to 'MultiThreadedDebugDLL'.

--- KKMachineLearning/GrayScaleImagesFV.cpp
+++ KKMachineLearning/GrayScaleImagesFV.cpp
@@ -605,9 +605,9 @@ GrayScaleImagesFVListPtr  GrayScaleImagesFVList::ExtractDuplicatesByRootImageFil
 
 
 GrayScaleImagesFVListPtr   GrayScaleImagesFVList::ExtractExamplesForAGivenClass (MLClassPtr  _mlClass,
-                                                                                   kkint32     _maxToExtract,
-                                                                                   float       _minSize
-                                                                                  )  const
+                                                                                 kkint32     _maxToExtract,
+                                                                                 float       _minSize
+                                                                                )  const
 {
   FeatureVectorListPtr  featureVectorList = FeatureVectorList::ExtractExamplesForAGivenClass (_mlClass, _maxToExtract, _minSize);
   GrayScaleImagesFVListPtr  imageFeaturesList = new GrayScaleImagesFVList (*featureVectorList);
